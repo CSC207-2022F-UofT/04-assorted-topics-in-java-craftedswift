4c085d243a4ff19e1bbd9f2f325bccea5b7449d7
Sanford Lin <craftedswift@gmail.com>
2022-10-09 20:57:08 -0400

    Finished all the TODOS:

--- src/main/java/DrivableMap.java
+++ src/main/java/DrivableMap.java
@@ -27,7 +27,13 @@ class DrivableMap {
      *       in drivable_map, then add the pair to drivable_map.
      *       Return true if the Drivable was added to drivable_map.
      */
-
+    public boolean addDrivable(String ID, Drivable object) {
+        if (!this.drivable_map.containsKey(ID)) {
+            this.drivable_map.put(ID, object);
+            return true;
+        }
+        return false;
+    }
 
 
 
@@ -37,6 +43,14 @@ class DrivableMap {
      * You may want to use drivable_map.keys() or drivable_map.values() to
      * iterate through drivable_map.
      */
+    public boolean hasFasterThan(int speed) {
+        for (Drivable driver: this.drivable_map.values()) {
+            if (driver.getMaxSpeed() >= speed) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 
 
@@ -46,7 +60,15 @@ class DrivableMap {
      *       returns a List containing all of the Tradable items in
      *       drivable_map.
      */
-
+    public List<Tradable> getTradable() {
+        List<Tradable> tradableList = new ArrayList<>();
+        for (Drivable driver: this.drivable_map.values()) {
+            if (driver instanceof Tradable) {
+                tradableList.add((Tradable) driver);
+            }
+        }
+        return tradableList;
+    }
 
 
     
